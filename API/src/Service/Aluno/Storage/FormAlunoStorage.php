222042698abae652b6efcd9fd489f7801f70631b
Carlos <carlos.falcon@ftdata.com.br>
2021-03-12 11:20:52 -0300

    Criaçaõ do Storage de Aluno

---
+++ API/src/Service/Aluno/Storage/FormAlunoStorage.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Service\Aluno\Storage;
+
+
+use App\Entity\Aluno;
+use Doctrine\ORM\EntityManager;
+
+class FormAlunoStorage
+{
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function save(Aluno $aluno = null)
+    {
+        if ($aluno){
+            $this->em->persist($aluno);
+            $this->em->flush();
+        }
+
+        return $aluno;
+    }
+
+    public function remove(Aluno $aluno = null)
+    {
+       if ($aluno){
+           $this->remove($aluno);
+           $this->em->flush();
+       }
+
+       return $aluno;
+    }
+
+    public function getAlunoPorId($data)
+    {
+        $id = (isset($data['id'])) ? $data['id'] : 0;
+
+        $alunoRepository = $this->em->getRepository(Aluno::class);
+        $aluno = $alunoRepository->find($id);
+
+        return is_null($aluno) ? [] : $aluno->jsonSerialize();
+    }
+}
