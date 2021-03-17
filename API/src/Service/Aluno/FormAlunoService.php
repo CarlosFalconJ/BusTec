551491770de190ad975beca2e45e1b6209f56ae3
Carlos <carlos.falcon@ftdata.com.br>
2021-03-17 11:17:51 -0300

    Criação da classe FormAlunoService que centraliza os métodos atualizar, cadastrar, apagar, buscarTodos, e seus métodos de apoios

---
+++ API/src/Service/Aluno/FormAlunoService.php
@@ -0,0 +1,135 @@
+<?php
+
+
+namespace App\Service\Aluno;
+
+
+use App\Entity\Aluno;
+use App\Helper\AppError;
+use App\Helper\ExtratorDadosDoRequest;
+use App\Helper\ResponseFactory;
+use App\Helper\ResponseHelper;
+use App\Service\Aluno\Storage\FormAlunoStorage;
+use App\Service\Aluno\Validatiom\AlunoValidation;
+use Doctrine\DBAL\Exception\ConnectionException;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Response;
+
+class FormAlunoService
+{
+
+    private $em;
+
+    private $extratorRequest;
+
+    public function __construct(EntityManager $em, ExtratorDadosDoRequest $extratorRequest)
+    {
+        $this->em = $em;
+        $this->extratorRequest = $extratorRequest;
+    }
+
+    public function cadastrar($dadosEmJson)
+    {
+        $formAlunoStorage = new FormAlunoStorage($this->em);
+
+        $alunoValidation = new AlunoValidation();
+
+        $parseAluno = new ParserAluno();
+
+        $formAluno = new RegraCadastrarAluno();
+        $formAluno->setAlunoValidation($alunoValidation);
+        $formAluno->setParseAluno($parseAluno);
+        $formAluno->setAlunoStorage($formAlunoStorage);
+
+        $aluno = $this->getNovoAluno();
+        if (!is_null($aluno)){
+            $alunoInfo = $formAluno->cadastrar($dadosEmJson, $aluno);
+        }
+
+        return $alunoInfo;
+    }
+
+    public function atualizar($dadosEmJson, $id)
+    {
+        $aluno = $this->getAluno($id);
+        $alunoExiste = !is_null($aluno);
+        $alunoInfo = null;
+
+        if ($alunoExiste){
+
+            $formAlunoStorage = new FormAlunoStorage($this->em);
+
+            $alunoValidation = new AlunoValidation();
+
+            $parseAluno = new ParserAluno();
+
+            $formAluno = new RegraAtualizarAluno();
+            $formAluno->setAlunoValidation($alunoValidation);
+            $formAluno->setParseAluno($parseAluno);
+            $formAluno->setAlunoStorage($formAlunoStorage);
+
+            $alunoInfo = $formAluno->atualizar($dadosEmJson, $aluno);
+        } else {
+            return new \Exception("Aluno inexistente", Response::HTTP_NOT_FOUND);
+        }
+
+        return $alunoInfo;
+    }
+
+    public function apagar($id)
+    {
+        $aluno = $this->getAluno($id);
+        $alunoExiste = !is_null($aluno);
+        $alunoInfo = null;
+
+        if (!$alunoExiste){
+            return new \Exception("Aluno inexistente", Response::HTTP_NOT_FOUND);
+        } else {
+            $formAlunoStorage = new FormAlunoStorage($this->em);
+
+            $formAluno = new RegraApagarAluno($formAlunoStorage);
+            $alunoInfo = $formAluno->apagar($aluno);
+        }
+        return $alunoInfo;
+    }
+
+    public function buscarTodos($request)
+    {
+        $ordenacao = $this->extratorRequest->buscadorDadosOrdenacao($request);
+
+        $filtro = $this->extratorRequest->bucadorDadosFiltro($request);
+
+        [$paginaAtual, $itensPorPagina] = $this->extratorRequest->infoPaginacao($request);
+
+        try{
+            $alunoRepository = $this->em->getRepository(Aluno::class);
+            $qntd = count($alunoRepository->findAll());
+            $lista = $alunoRepository->findBy($filtro, $ordenacao,$itensPorPagina,($paginaAtual-1)* $itensPorPagina);
+
+        }catch (ConnectionException $error){
+            throw new \Exception('Erro ao se conectar ao banco de dados, verifique a existencia do mesmo', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $responseFactory = new ResponseHelper(true, $lista, Response::HTTP_OK, $qntd, $paginaAtual, $itensPorPagina);
+
+        return $responseFactory->getResponse();
+    }
+    
+    public function getAluno($id)
+    {
+        $id = isset($id) ? $id : 0;
+
+        $sistemaAlunoRepository = $this->em->getRepository(Aluno::class);
+        $aluno = $sistemaAlunoRepository->find($id);
+
+        return$aluno;
+    }
+
+    public function getNovoAluno()
+    {
+        $aluno = new Aluno();
+
+        return $aluno;
+
+    }
+}
