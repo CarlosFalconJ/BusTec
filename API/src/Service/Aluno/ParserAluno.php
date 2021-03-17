4a212fdb6d2a1e8b09b2059c64af0e97368e7e22
Carlos <carlos.falcon@ftdata.com.br>
2021-03-17 11:10:46 -0300

    Criação do ParserAluno e o método setAlunoFromDAta

--- API/src/Service/Aluno/ParserAluno.php
+++ API/src/Service/Aluno/ParserAluno.php
@@ -4,7 +4,25 @@
 namespace App\Service\Aluno;
 
 
+use App\Entity\Aluno;
+
+
+use Doctrine\ORM\EntityManager;
+
+
 class ParserAluno
 {
+    public function setAlunoFromData($dadosJson, Aluno $aluno)
+    {
 
+        $aluno->setNome($dadosJson->nome);
+        $aluno->setNumeroContato($dadosJson->numero_contato);
+        $aluno->setEmail($dadosJson->email);
+        $aluno->setRa($dadosJson->ra);
+        $aluno->setBairro($dadosJson->bairro);
+        $aluno->setRua($dadosJson->rua);
+        $aluno->setNumeroCasa($dadosJson->numero_casa);
+
+        return $aluno;
+    }
 }
