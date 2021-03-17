f81ee5a98d9e6cbaa9756febd289456bce5e9296
Carlos <carlos.falcon@ftdata.com.br>
2021-03-17 11:19:12 -0300

    Criação da classe ExtratorDadosRequest para auxilar na hora de mostrar a busca de todos os contatos

---
+++ API/src/Helper/ExtratorDadosDoRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Helper;
+
+
+use Symfony\Component\HttpFoundation\Request;
+
+class ExtratorDadosDoRequest
+{
+    public function buscadorDadosRequest(Request $request)
+    {
+        $allQuerys = $request->query->all();
+
+        $infoOrdenacao = array_key_exists('sort', $allQuerys) ? $allQuerys['sort'] : null;
+        unset($allQuerys['sort']);
+
+        $paginaAtual = array_key_exists('page', $allQuerys) ? $allQuerys['page'] : 1;
+        unset($allQuerys['page']);
+
+        $itensPorPagina = array_key_exists('itensPorPagina', $allQuerys) ? $allQuerys['itensPorPagina'] : 10;
+        unset($allQuerys['itensPorPagina']);
+
+        return [$infoOrdenacao, $allQuerys, $paginaAtual, $itensPorPagina];
+    }
+
+    public function buscadorDadosOrdenacao(Request $request)
+    {
+        [$infoOrdenacao, ] = $this->buscadorDadosRequest($request);
+
+        return $infoOrdenacao;
+    }
+
+    public function bucadorDadosFiltro(Request $request)
+    {
+        [, $filtro] = $this->buscadorDadosRequest($request);
+
+        return $filtro;
+    }
+
+    public function infoPaginacao(Request $request)
+    {
+        [, ,$paginaAtual,$itensPorPagina] = $this->buscadorDadosRequest($request);
+
+        return [$paginaAtual, $itensPorPagina];
+    }
+}
