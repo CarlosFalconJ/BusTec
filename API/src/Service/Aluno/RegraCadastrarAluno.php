f747759360a4cd32bfb609bb1d66fe435ca5de15
Carlos <carlos.falcon@ftdata.com.br>
2021-03-17 11:14:33 -0300

    Criação da classe RegraCadastrarAluno e seus métodos para setar os status atuais da validation, do parser e storage e o método cadastrar

---
+++ API/src/Service/Aluno/RegraCadastrarAluno.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Service\Aluno;
+
+
+use App\Entity\Aluno;
+use App\Service\Aluno\Storage\FormAlunoStorage;
+use App\Service\Aluno\Validatiom\AlunoValidation;
+
+class RegraCadastrarAluno
+{
+    private $storage;
+
+    private $alunoValidation;
+
+    private $parser;
+
+    public function setAlunoValidation(AlunoValidation $validation)
+    {
+        $this->alunoValidation = $validation;
+    }
+
+    public function setParseAluno(ParserAluno $parser)
+    {
+        $this->parser = $parser;
+    }
+
+    public function setAlunoStorage(FormAlunoStorage $storage)
+    {
+        $this->storage = $storage;
+    }
+
+    public function cadastrar($dadosEmJson, Aluno $aluno)
+    {
+        $dadosEmJsonIsValid = $this->alunoValidation->validate($dadosEmJson);
+        $alunoInfo = [];
+
+        if ($dadosEmJsonIsValid){
+            $this->parser->setAlunoFromData($dadosEmJson, $aluno);
+            $this->storage->save($aluno);
+            $alunoInfo = $aluno->jsonSerialize();
+        }
+
+        return$alunoInfo;
+    }
+}
