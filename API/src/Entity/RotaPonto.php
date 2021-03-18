0767b8adc5e1d3f7e0f34f0db8d327f645be53fe
Carlos <carlos.falcon@ftdata.com.br>
2021-03-18 13:25:53 -0300

    Criação da entidade RotaPonto

---
+++ API/src/Entity/RotaPonto.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RotaPontoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RotaPontoRepository::class)
+ * @ORM\Table(name="rota_ponto")
+ */
+class RotaPonto
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Rota")
+     * @ORM\JoinColumn(name="id_rota", referencedColumnName="id")
+     */
+    private $rota;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Ponto")
+     * @ORM\JoinColumn(name="id_ponto", referencedColumnName="id")
+     */
+    private $ponto;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRota()
+    {
+        return $this->rota;
+    }
+
+    public function setRota($rota): self
+    {
+        $this->rota = $rota;
+        return $this;
+    }
+
+    public function getPonto()
+    {
+        return $this->ponto;
+    }
+
+    public function setPonto($ponto): self
+    {
+        $this->ponto = $ponto;
+        return $this;
+    }
+}
