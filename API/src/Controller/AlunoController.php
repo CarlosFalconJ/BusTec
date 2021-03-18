d17eaa3f23e9d3c7e4689638f5cccfbedb1de588
Carlos <carlos.falcon@ftdata.com.br>
2021-03-18 13:31:10 -0300

    Criação do controller AlunoCOntroller

---
+++ API/src/Controller/AlunoController.php
@@ -0,0 +1,73 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Helper\ExtratorDadosDoRequest;
+use App\Helper\ResponseHelper;
+use App\Service\Aluno\FormAlunoService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AlunoController
+{
+    private $em;
+
+    private $dadosDoRequest;
+
+    public function __construct(EntityManagerInterface $em, ExtratorDadosDoRequest $dadosDoRequest)
+    {
+        $this->em = $em;
+        $this->dadosDoRequest = $dadosDoRequest;
+    }
+
+    public function insertAluno(int $id_onibus, Request $request)
+    {
+        $dadosEmJSon = json_decode($request->getContent());
+
+        $formAlunoService = new FormAlunoService($this->em, $this->dadosDoRequest);
+        $aluno = $formAlunoService->cadastrar($id_onibus, $dadosEmJSon);
+
+        $response = new ResponseHelper(true, $aluno, Response::HTTP_CREATED );
+        return $response->getResponse();
+    }
+
+    public function buscarTodosAlunos(Request $request)
+    {
+       $formAlunoService =  new FormAlunoService($this->em, $this->dadosDoRequest);
+       $response  = $formAlunoService->buscarTodos($request);
+
+       return $response;
+    }
+
+    public function updateAluno(int $id, Request $request)
+    {
+        $dadosEmJson = json_decode($request->getContent());
+
+        $formAlunoService = new FormAlunoService($this->em, $this->dadosDoRequest);
+        $aluno =$formAlunoService->atualizar($dadosEmJson, $id);
+
+        $response = new ResponseHelper(true, $aluno, Response::HTTP_OK );
+        return $response->getResponse();
+    }
+
+    public function deleteAluno($id)
+    {
+        $formAlunoService = new FormAlunoService($this->em, $this->dadosDoRequest);
+        $aluno = $formAlunoService->apagar($id);
+
+        $response = new ResponseHelper(true, $aluno, Response::HTTP_OK );
+        return $response->getResponse();
+    }
+
+    public function buscarAluno($id)
+    {
+        $formAlunoService = new FormAlunoService($this->em, $this->dadosDoRequest);
+        $aluno = $formAlunoService->getAluno($id);
+
+        $response = new ResponseHelper(true, $aluno, Response::HTTP_OK );
+        return $response->getResponse();
+    }
+}
