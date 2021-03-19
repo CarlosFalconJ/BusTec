b81b7f79bf27109b9bd7947f9bbdc66b6a67006a
Carlos <carlos.falcon@ftdata.com.br>
2021-03-19 11:55:39 -0300

    Criação do ParserPonto

---
+++ API/src/Service/Ponto/ParserPonto.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Service\Ponto;
+
+
+use App\Entity\Ponto;
+
+class ParserPonto
+{
+    public function setPontoFromData($dadosJson, Ponto $ponto)
+    {
+        $ponto->setNome($dadosJson->nome);
+        $ponto->setBairro($dadosJson->bairro);
+        $ponto->setRua($dadosJson->rua);
+        $ponto->setPontoReferencia($dadosJson->ponto_referencia);
+
+        return $ponto;
+    }
+}
