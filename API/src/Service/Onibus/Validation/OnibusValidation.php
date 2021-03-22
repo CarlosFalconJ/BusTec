830d9ff612e330d2982dbc9430fc4240759b02ad
Carlos <carlos.falcon@ftdata.com.br>
2021-03-22 11:43:21 -0300

    Mudança nas validações

--- API/src/Service/Onibus/Validation/OnibusValidation.php
+++ API/src/Service/Onibus/Validation/OnibusValidation.php
@@ -8,11 +8,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class OnibusValidation
 {
-    public function validate($dadosJson)
+    public function validate($dadosEmJson)
     {
-        if (strlen($dadosJson->placa) <= 6){
+        if (strlen($dadosEmJson->placa) <= 6){
              throw new \Exception("O campo (placa) é inválido", Response::HTTP_BAD_REQUEST);
-        }elseif (strlen($dadosJson->motorista_responsavel) <=2){
+        }elseif (strlen($dadosEmJson->motorista_responsavel) <=2){
             throw new \Exception("O campo (mototista_responsavel) é inválido", Response::HTTP_BAD_REQUEST);
         }
 
