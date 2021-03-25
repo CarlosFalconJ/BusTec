52fed8b2b18f92fd666bccfbe3d1acdc036b92f3
Carlos <carlos.falcon@ftdata.com.br>
2021-03-25 10:29:00 -0300

    Adição do Storage

---
+++ API/src/Service/RotaPonto/RotaPontoStorage/FormRotaPontoStorage.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Service\RotaPonto\RotaPontoStorage;
+
+
+use App\Entity\RotaPonto;
+use Doctrine\ORM\EntityManager;
+
+class FormRotaPontoStorage
+{
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function save(RotaPonto $rota_ponto = null)
+    {
+        if ($rota_ponto){
+            $this->em->persist($rota_ponto);
+            $this->em->flush();
+        }
+
+        return $rota_ponto;
+    }
+
+    public function remove(RotaPonto $rota_ponto = null)
+    {
+        if ($rota_ponto){
+            $this->em->remove($rota_ponto);
+            $this->em->flush();
+        }
+
+        return $rota_ponto;
+    }
+}
