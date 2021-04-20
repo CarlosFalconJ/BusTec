560c0f9f3453e90446ac6ded96cc1ea795b10caa
Carlos <carlos.falcon@ftdata.com.br>
2021-04-20 09:09:41 -0300

    Adicionado Service web

---
+++ API/src/Service/Onibus/OnibusService.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Service\Onibus;
+
+
+use App\Repository\OnibusRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class OnibusService
+{
+    private $em;
+
+    private $onibusRepository;
+
+    public function __construct(EntityManagerInterface $em, OnibusRepository $onibusRepository)
+    {
+        $this->em = $em;
+        $this->onibusRepository = $onibusRepository;
+    }
+
+    public function listarOnibus(){
+
+        $alunos = $this->onibusRepository->buscarTodosAlunos();
+        return $alunos;
+    }
+
+}
