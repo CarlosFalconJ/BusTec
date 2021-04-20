bc4c5dda4dd32b9300d47ec47d7779e207fca68d
Carlos <carlos.falcon@ftdata.com.br>
2021-04-20 11:31:27 -0300

    Renderizador das telas de rotas

---
+++ API/src/Controller/BustecAdm/RotasRender.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Controller\BustecAdm;
+
+
+use App\Repository\RotaRepository;
+use App\Service\Rota\RotaService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class RotasRender extends  AbstractController
+{
+    private $em;
+
+    private $rotaRepository;
+
+    public function __construct(EntityManagerInterface $em, RotaRepository $rotaRepository)
+    {
+
+        $this->em = $em;
+        $this->rotaRepository = $rotaRepository;
+    }
+
+    public function cadastroRotas()
+    {
+        return $this->render('Rota\cadastro_rota.html.twig');
+    }
+
+    public function atualizarRotas()
+    {
+        return $this->render('Rota\atualizar_rotas.html.twig');
+    }
+
+    public function listarRotas()
+    {
+        $rotaService = new RotaService($this->em, $this->rotaRepository);
+        $rotas = $rotaService->listarRotas();
+
+        return $this->render('Rota\listar_rotas.html.twig', ["rotas" => $rotas]);
+    }
+}
