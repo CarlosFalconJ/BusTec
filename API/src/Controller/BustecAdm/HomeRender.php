2eb08b0541c0dc9c535bf0affdbd58c2d43c56c1
Carlos <carlos.falcon@ftdata.com.br>
2021-04-30 10:07:17 -0300

    Render Home ;

--- API/src/Controller/BustecAdm/HomeRender.php
+++ API/src/Controller/BustecAdm/HomeRender.php
@@ -4,12 +4,55 @@
 namespace App\Controller\BustecAdm;
 
 
+
+use App\Repository\AlunoRepository;
+use App\Repository\OnibusRepository;
+use App\Repository\PontoRepository;
+use App\Repository\RotaRepository;
+use App\Service\home\FormHomeService;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class HomeRender extends AbstractController
 {
+
+    private $onibusRepository;
+
+    private $em;
+
+    private $alunoRepository;
+
+    private $rotaRepository;
+    /**
+     * @var PontoRepository
+     */
+    private $pontoRepository;
+
+    public function __construct(EntityManagerInterface $em, OnibusRepository $onibusRepository, AlunoRepository $alunoRepository, RotaRepository $rotaRepository, PontoRepository $pontoRepository)
+    {
+        $this->em = $em;
+        $this->onibusRepository = $onibusRepository;
+        $this->alunoRepository = $alunoRepository;
+        $this->rotaRepository = $rotaRepository;
+        $this->pontoRepository = $pontoRepository;
+    }
+
     public function home()
     {
         return $this->render('Home\home.html.twig');
     }
+
+    public function buscaTotais()
+    {
+       $homeService = New FormHomeService($this->em);
+       $totalOnibus = $homeService->buscaTotalOnibus($this->onibusRepository);
+       $totalAlunos = $homeService->buscaTotalAlunos($this->alunoRepository);
+       $totalRotas  = $homeService->buscaTotalRotas($this->rotaRepository);
+       $totalPontos = $homeService->buscaTotalPontos($this->pontoRepository);
+       return $this->render('Home\home.html.twig',['totalOnibus' => $totalOnibus, 'totalAlunos' => $totalAlunos, 'totalRotas' => $totalRotas, 'totalPontos' => $totalPontos]);
+    }
+
+
 }
