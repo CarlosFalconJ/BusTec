d31cde89da5b6c7b231ef8f2038a1c681aba57bb
Carlos <carlos.falcon@ftdata.com.br>
2021-05-04 12:15:51 -0300

    Hender login que faz a validação se ouver um token com a chave 'access_token' ela a remove toda vez que é chamado

--- API/src/Controller/BustecAdm/LoginRender.php
+++ API/src/Controller/BustecAdm/LoginRender.php
@@ -4,12 +4,27 @@
 namespace App\Controller\BustecAdm;
 
 
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class LoginRender extends AbstractController
 {
-    public function login()
+    public function login(Request $request)
     {
-        return $this->render('login\index.html.twig');
+
+        $response = new Response();
+
+        if ($request->cookies->get('access_token')){
+            $response->headers->clearCookie('access_token');
+            $response->send();
+        }
+
+        return $this->render('login\index.html.twig', [], $response);
     }
+
 }
