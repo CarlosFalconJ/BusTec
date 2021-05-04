ccbd78755c5af2c9a111ba630ad4a7a55c0067f5
Carlos <carlos.falcon@ftdata.com.br>
2021-05-04 12:14:40 -0300

    Adição do hender da homeque chama querys para mostrar totais

--- API/src/Controller/BustecAdm/HomeRender.php
+++ API/src/Controller/BustecAdm/HomeRender.php
@@ -9,10 +9,19 @@ use App\Repository\AlunoRepository;
 use App\Repository\OnibusRepository;
 use App\Repository\PontoRepository;
 use App\Repository\RotaRepository;
+use App\Security\JwtAutenticador;
 use App\Service\home\FormHomeService;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use http\Cookie;
+use http\Env\Response;
+use http\Header;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\HeaderUtils;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Twig\Token;
 
 
 class HomeRender extends AbstractController
@@ -39,19 +48,18 @@ class HomeRender extends AbstractController
         $this->pontoRepository = $pontoRepository;
     }
 
-    public function home()
+    public function buscaTotais(Request $request)
     {
-        return $this->render('Home\home.html.twig');
-    }
 
-    public function buscaTotais()
-    {
+
        $homeService = New FormHomeService($this->em);
        $totalOnibus = $homeService->buscaTotalOnibus($this->onibusRepository);
        $totalAlunos = $homeService->buscaTotalAlunos($this->alunoRepository);
        $totalRotas  = $homeService->buscaTotalRotas($this->rotaRepository);
        $totalPontos = $homeService->buscaTotalPontos($this->pontoRepository);
-       return $this->render('Home\home.html.twig',['totalOnibus' => $totalOnibus, 'totalAlunos' => $totalAlunos, 'totalRotas' => $totalRotas, 'totalPontos' => $totalPontos]);
+
+
+        return $this->render('Home\home.html.twig',['totalOnibus' => $totalOnibus, 'totalAlunos' => $totalAlunos, 'totalRotas' => $totalRotas, 'totalPontos' => $totalPontos]);
     }
 
 
