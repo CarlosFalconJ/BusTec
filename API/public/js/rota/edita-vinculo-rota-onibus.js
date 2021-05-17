8e09fe6d269f2a380264478e2f6419ae3c7500e2
Carlos <carlos.falcon@ftdata.com.br>
2021-05-17 11:47:51 -0300

    Adicionado js  atualizar rota - onibus

---
+++ API/public/js/rota/edita-vinculo-rota-onibus.js
@@ -0,0 +1,33 @@
+$("#atualizar_vinculo-rota-onibus").click(function (e) {
+    e.preventDefault();
+
+    let id = window.location.href;
+    id = id.split('/');
+    let idVinculo = id[4];
+
+    console.log(idVinculo)
+
+    var idRota = $(".atualizar_vinculo-rota-o").val();
+    var idOnibus = $(".atualizar_vinculo-onibus").val();
+
+    realizaRequestAtualizaRotaOnibus(idVinculo, idRota, idOnibus);
+});
+
+function realizaRequestAtualizaRotaOnibus(idVinculo, idRota, idOnibus) {
+    $.ajax({
+        type: 'PUT',
+        url: '/vinculo/' + idVinculo + '/rota/' + idRota +  '/onibus/' + idOnibus,
+        contentType: 'application/json',
+    }).done(function () {
+        menssagemDeSucesso('Vínculo atualizado com sucesso!!', '/rota/vinculos/' + idRota);
+    }).fail(function (msg) {
+        menssagemDeErro('Vínculo não atualizado!!, verifique os dados e tente novamente');
+    });
+}
+
+$("#limpar_vinculo-rota-onibus").click(function (e) {
+    e.preventDefault();
+
+    $(".atualizar_vinculo-rota-o").val("Selecione a rota");
+    $(".atualizar_vinculo-onibus").val("Selecione o ônibus");
+});
