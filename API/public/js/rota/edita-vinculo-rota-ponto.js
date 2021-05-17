38c88cb972e8316272ae7dacb8de1451217b6a5f
Carlos <carlos.falcon@ftdata.com.br>
2021-05-17 10:52:39 -0300

    Adcionado js editar vinculo rota - ponto

---
+++ API/public/js/rota/edita-vinculo-rota-ponto.js
@@ -0,0 +1,36 @@
+$("#atualizar_vinculo-rota-ponto").click(function (e) {
+    e.preventDefault();
+
+    let id = window.location.href;
+    id = id.split('/');
+    let idVinculo = id[4];
+
+    console.log(idVinculo);
+    var idRota = $(".atualiza_vinculo-rota").val();
+    var idPonto = $(".atualizar_vinculo-ponto").val();
+    var data = $("#datetime-atualizar_rota-ponto").val();
+
+    var json =  serializadorDate(data);
+
+    realizaRequestAtualizaRotaPonto(idVinculo, idRota, idPonto, json);
+});
+
+function realizaRequestAtualizaRotaPonto(idVinculo, idRota, idPonto, json) {
+    $.ajax({
+        type: 'PUT',
+        url: '/vinculo/' + idVinculo + '/rota/' + idRota +  '/ponto/' + idPonto,
+        contentType: 'application/json',
+        data: json
+    }).done(function () {
+        menssagemDeSucesso('Vínculo atualizado com sucesso!!', '/rota/vinculos/' + idRota);
+    }).fail(function (msg) {
+        menssagemDeErro('Vínculo não atualizado!!, verifique os dados e tente novamente');
+    });
+}
+
+$("#limpar_vinculo-rota-ponto_atualizar").click(function (e) {
+    e.preventDefault();
+
+    $(".escolha_vinculo-rota-o").val("Selecione a rota");
+    $(".escolha_vinculo-ponto").val("Selecione o ponto");
+});
