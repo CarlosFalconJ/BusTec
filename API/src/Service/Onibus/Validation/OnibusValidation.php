760359fd256bfea65e1f78004c428ab6a65723eb
Carlos <carlos.falcon@ftdata.com.br>
2021-05-19 11:20:07 -0300

    Mudança na validação dos dados do onibus

--- API/src/Service/Onibus/Validation/OnibusValidation.php
+++ API/src/Service/Onibus/Validation/OnibusValidation.php
@@ -10,9 +10,9 @@ class OnibusValidation
 {
     public function validate($dadosEmJson)
     {
-        if (strlen($dadosEmJson->placa) <= 6){
+        if (!preg_match('^[A-Z]{3}[0-9]{4}$^',$dadosEmJson->placa)){
              throw new \Exception("O campo (placa) é inválido", Response::HTTP_BAD_REQUEST);
-        }elseif (strlen($dadosEmJson->motorista_responsavel) <=2){
+        }elseif (strlen($dadosEmJson->motorista_responsavel) == 0){
             throw new \Exception("O campo (mototista_responsavel) é inválido", Response::HTTP_BAD_REQUEST);
         }
 
