26aa7793400613c5994d1fa18e1db123de129adb
Carlos <carlos.falcon@ftdata.com.br>
2021-05-19 11:20:50 -0300

    Mudança na validação dos dados do ponto

--- API/src/Service/Ponto/Validation/PontoValidation.php
+++ API/src/Service/Ponto/Validation/PontoValidation.php
@@ -10,13 +10,13 @@ class PontoValidation
 {
     public function validate($dadosEmJson)
     {
-        if (strlen($dadosEmJson->nome) <= 2){
+        if (strlen($dadosEmJson->nome) == 0){
             throw new \Exception("O campo (nome) é inválido", Response::HTTP_BAD_REQUEST);
-        }elseif (strlen($dadosEmJson->bairro) <= 2){
+        }elseif (strlen($dadosEmJson->bairro) == 0){
             throw new \Exception("O campo (bairro) é inválido",Response::HTTP_BAD_REQUEST);
-        }elseif (strlen($dadosEmJson->rua) <= 2){
+        }elseif (strlen($dadosEmJson->rua) == 0){
             throw new \Exception("O campo (rua) é inválido",Response::HTTP_BAD_REQUEST);
-        }elseif (strlen($dadosEmJson->ponto_referencia) <= 2){
+        }elseif (strlen($dadosEmJson->ponto_referencia) == 0){
             throw new \Exception("O campo (ponto_referencia) é inválido",Response::HTTP_BAD_REQUEST);
         }
 
