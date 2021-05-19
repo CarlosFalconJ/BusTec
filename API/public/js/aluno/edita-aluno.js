ec231619e7401be39f210798b4c5a425b28e83fb
Carlos <carlos.falcon@ftdata.com.br>
2021-05-19 14:24:34 -0300

    Pega dados do cadastro  e mostra na hora de atualizar

--- API/public/js/aluno/edita-aluno.js
+++ API/public/js/aluno/edita-aluno.js
@@ -1,4 +1,15 @@
-﻿
+﻿$("#atualzia-aluno").ready(function (){
+
+    $("#nome_aluno-atualizar").val(localStorage.getItem('atualizar_nome_aluno'));
+    $("#numero_aluno-atualizar").val(localStorage.getItem('atualiza_numero_aluno'));
+    $("#email_aluno-atualizar").val(localStorage.getItem( 'email_aluno-atualizar'));
+    $("#ra_aluno-atualizar").val(localStorage.getItem    ('ra_aluno-atualizar'));
+    $("#bairro_aluno-atualizar").val(localStorage.getItem('bairro_aluno-atualizar'));
+    $("#rua_aluno-atualizar").val(localStorage.getItem   ('rua_aluno-atualizar'));
+    $("#numero_casa_aluno-atualizar").val(localStorage.getItem('numero_casa_aluno-atualizar'));
+    $(".escolha-onibus").val(localStorage.getItem('escolha_onibus'));
+})
+
 $("#salvar-aluno-atualizar").click(function (e) {
     e.preventDefault();
 
@@ -6,12 +17,12 @@ $("#salvar-aluno-atualizar").click(function (e) {
     id = id.split('/');
     let idAluno = id[5]
 
-    var nome = $("#nome_aluno-atualizar").val();
-    var numero = $("#numero_aluno-atualizar").val();
-    var email = $("#email_aluno-atualizar").val();
-    var ra = $("#ra_aluno-atualizar").val();
-    var bairro = $("#bairro_aluno-atualizar").val();
-    var rua = $("#rua_aluno-atualizar").val();
+    var nome =     $("#nome_aluno-atualizar").val();
+    var numero =   $("#numero_aluno-atualizar").val();
+    var email =    $("#email_aluno-atualizar").val();
+    var ra =       $("#ra_aluno-atualizar").val();
+    var bairro =   $("#bairro_aluno-atualizar").val();
+    var rua =      $("#rua_aluno-atualizar").val();
     var num_casa = $("#numero_casa_aluno-atualizar").val();
     var idOnibus = $(".escolha-onibus").val();
 
